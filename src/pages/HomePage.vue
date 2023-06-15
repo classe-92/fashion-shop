278bf2f3c298fdb6bf1736cb0a18968b71995155
klelia <clelia.fradella@gmail.com>
2023-06-15 20:46:50 +0200

    slider

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -19,35 +19,42 @@
             </div>
 
         </div>
-        <!-- <div id="carouselExampleCaptions" class="carousel slide">
-            <div class="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
-                    aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
-                    aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
-                    aria-label="Slide 3"></button>
-            </div>
-            <div class="carousel-inner">
-                <div class="carousel-item active" v-for="">
-                    <img :src="store.imgBasePath + products[0].cover_image" class="d-block w-100" alt="...">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>First slide label</h5>
-                        <p>Some representative placeholder content for the first slide.</p>
+        <div class="row justify-content-center my-4">
+            <div class="col-12 col-lg-8">
+                <div id="productCarousel" class="carousel carousel-dark slide">
+                    <div class="carousel-indicators">
+                        <button type="button" v-for="(product, index) in products" :key="product.id"
+                            data-bs-target="#productCarousel" :class="{ 'active': activeSlide === index }">
+                        </button>
                     </div>
+                    <div class="carousel-inner">
+                        <Transition :name="direction">
+                            <div class="carousel-item active" :key="products[activeSlide].id">
+                                <img :src="store.imgBasePath + products[activeSlide].cover_image" class="d-block"
+                                    alt="..." />
+
+
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>{{ products[activeSlide].name }}</h5>
+                                    <!-- <p>{{ product.descrption }}</p> -->
+                                </div>
+                            </div>
+                        </Transition>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#productCarouselCaptions"
+                        data-bs-slide="prev" @click="prev()">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="##productCarouselCaptions"
+                        data-bs-slide="next" @click="next()">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
                 </div>
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
-                data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
-                data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
-        </div> -->
+        </div>
+
     </section>
 </template>
 
@@ -65,7 +72,9 @@ export default {
             store,
             loading: true,
             products: [],
-            categories: []
+            categories: [],
+            activeSlide: 0,
+            direction: 'next'
         }
     },
     methods: {
@@ -79,6 +88,14 @@ export default {
             }).finally(() => {
                 this.loading = false;
             });
+        },
+        next() {
+            this.direction = 'next';
+            this.activeSlide = this.activeSlide < this.products.length - 1 ? this.activeSlide + 1 : 0;
+        },
+        prev() {
+            this.direction = 'prev';
+            this.activeSlide = this.activeSlide > 0 ? this.activeSlide - 1 : this.products.length - 1;
         }
     },
     mounted() {
